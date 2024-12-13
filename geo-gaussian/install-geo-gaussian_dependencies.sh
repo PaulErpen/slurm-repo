c9aee12276eabbec8505e4f260df074c3de42d2e
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-12-13 10:52:28 +0100

    Add simple ls

--- geo-gaussian/install-geo-gaussian_dependencies.sh
+++ geo-gaussian/install-geo-gaussian_dependencies.sh
@@ -4,7 +4,9 @@
 cd repos
 rm -rf GeoGaussian-capped
 git clone https://github.com/PaulErpen/GeoGaussian-capped.git
+ls
 
 # install cuda kernels
 cd GeoGaussian-capped/submodules/diff-gaussian-rasterization
+ls
 python3 -m pip install --target="../../../../libs/geogaussian" --upgrade -v --no-deps .
