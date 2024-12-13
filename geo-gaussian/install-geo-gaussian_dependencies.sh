e25baed675a9bd701debd861c88f78b3ef18a72e
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-12-13 10:35:09 +0100

    Try to install first dependency

--- geo-gaussian/install-geo-gaussian_dependencies.sh
+++ geo-gaussian/install-geo-gaussian_dependencies.sh
@@ -1,2 +1,10 @@
-# install the dependencies here and compile the cuda kernel
-# really make sure that you only write to the correct lib
+#!/bin/bash
+
+# Reset repo
+cd repos
+rm -rf GeoGaussian-capped
+git clone https://github.com/PaulErpen/GeoGaussian-capped.git
+
+# install cuda kernels
+cd GeoGaussian-capped/submodules/diff-gaussian-rasterization
+python3 -m pip install --target="../../../../libs/geogaussian"
